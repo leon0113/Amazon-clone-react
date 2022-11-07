3d45840e788caac92fb85dd31ea379449973f6e1
leon0113 <tahjib.leon399@gmail.com>
2022-11-08 00:50:45 +0600

    'Cart-component'

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,17 +1,21 @@
 import React, { useEffect, useState } from 'react';
+import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css'
 
 
 const Shop = () => {
     const [products, setProducts] = useState([]);
-    const [cart, setCart] = useState([]);
 
     useEffect(() => {
         fetch('products.json')
             .then(res => res.json())
             .then(data => setProducts(data))
     }, []);
+
+
+    const [cart, setCart] = useState([]);
+
     const handleAddToClick = (product) => {
         // console.log(product);
         const newCart = [...cart, product];
@@ -29,8 +33,7 @@ const Shop = () => {
                 }
             </div>
             <div className="cart-container">
-                <h4>Order Summary</h4>
-                <p>Selected Items : {cart.length}</p>
+                <Cart cart={cart}></Cart>
             </div>
         </div>
     );
